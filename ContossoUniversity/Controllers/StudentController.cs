204e72e3872893d0ffc51bc2b60275da098ac5f2
Seth Johnson <sethjdesigns@gmail.com>
2015-04-21 09:51:00 -0400

    email data bind and view work on submit

--- ContossoUniversity/Controllers/StudentController.cs
+++ ContossoUniversity/Controllers/StudentController.cs
@@ -47,7 +47,7 @@ namespace ContossoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
+        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate, EmailAddress")]Student student)
         {
             try
             {
