d06752bfa2f16413868d82371231ecf4f700275a
Seth Johnson <sethjdesigns@gmail.com>
2015-04-21 10:32:07 -0400

    addded try/catch to edit method

--- ContossoUniversity/Controllers/StudentController.cs
+++ ContossoUniversity/Controllers/StudentController.cs
@@ -86,14 +86,23 @@ namespace ContossoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public ActionResult Edit([Bind(Include = "ID, LastName, FirstMidName, EnrollmentDate, EmailAddress")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
+            catch (DataException /* dex */)
+            { 
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
             return View(student);
         }
 
