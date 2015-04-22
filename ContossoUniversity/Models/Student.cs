46aa16404e402abf0651b7997fca1d74ca295618
Seth Johnson <sethjdesigns@gmail.com>
2015-04-22 10:10:32 -0400

    updated css, and added sort functionality

--- ContossoUniversity/Models/Student.cs
+++ ContossoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace ContossoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [Display(Name = "Enrollment Date:"), DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
