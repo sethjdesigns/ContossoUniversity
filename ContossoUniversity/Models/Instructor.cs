d6973b6bec3ccdefaddb3c2c5b69cc50d3ddd770
Seth Johnson <sethjdesigns@gmail.com>
2015-04-24 09:47:04 -0400

    added officeassignment and instructor models

--- ContossoUniversity/Models/Instructor.cs
+++ ContossoUniversity/Models/Instructor.cs
@@ -2,29 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace ContosoUniversity.Models
+namespace ContossoUniversity.Models
 {
  public class Instructor
     {
         public int ID { get; set; }
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
+        [Required, Display(Name = "Last Name"), StringLength(50)]
         public string LastName { get; set; }
-        [Required]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        [StringLength(50)]
+        [Required, Column("FirstName"), Display(Name = "First Name"), StringLength(50)]
         public string FirstMidName { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Hire Date")]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
         [Display(Name = "Full Name")]
         public string FullName
-        {
-            get { return LastName + ", " + FirstMidName; }
-        }
+            {
+               get { return LastName + ", " + FirstMidName; }
+            }
 
         public virtual ICollection<Course> Courses { get; set; }
 
